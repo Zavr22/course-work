0bce7c45dace36cd040cd0665f3a4c155d5ba2bd
norog6 <92385720+norog6@users.noreply.github.com>
2022-10-28 20:10:27 +0300

    [SIGNUP]

--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -3,12 +3,39 @@ package com.example.demo4;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button autorithationButton;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize() {
+        assert autorithationButton != null : "fx:id=\"autorithationButton\" was not injected: check your FXML file 'Untitled'.";
+        assert login_field != null : "fx:id=\"login_field\" was not injected: check your FXML file 'Untitled'.";
+        assert password_field != null : "fx:id=\"password_field\" was not injected: check your FXML file 'Untitled'.";
+        assert signUpButton != null : "fx:id=\"signUpButton\" was not injected: check your FXML file 'Untitled'.";
+
     }
 }
