3030c9955c956e3804a9d07e92167e2abe39ce84
Zavr22 <Mkulik2407@gmail.com>
2022-10-30 00:19:50 +0300

    [ADD] DataBase config

---
+++ src/main/java/com/example/demo4/Constants.java
@@ -0,0 +1,63 @@
+package com.example.demo4;
+
+public class Constants {
+    public static final String USER_TABLE = "User";
+    public static final int USER_ID = Integer.parseInt("idUser");
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_NAME = "name";
+    public static final String USER_SURNAME = "surname";
+
+    public static final String TEAMS_TABLE = "Teams";
+    public static final int TEAMS_ID = Integer.parseInt("idTeams");
+    public static final String TEAMS_NAME = "name";
+    public static final String TEAMS_SPORT = "sport";
+
+
+    public static final String BETS_TABLE = "bet";
+    public static final int BET_EVENT_ID = Integer.parseInt("idEvent");
+    public static final int BETTYPE_ID = Integer.parseInt("betType");
+    public static final int BETSUM_ID = Integer.parseInt("betSum");
+    public static final int BET_ID = Integer.parseInt("idt");
+    public static final int BETTER_ID = Integer.parseInt("betterId");
+
+    public static final String BETTYPES_TABLE = "betTypes";
+    public static final int BETTYPES_ID = Integer.parseInt("id");
+    public static final String BETTYPES_NAME = "name";
+
+    public static final String KOEFS_TABLE = "koefs";
+    public static final int KOEFS_ID = Integer.parseInt("id");
+    public static final int KOEFS_EVENT_ID = Integer.parseInt("idEvent");
+    public static final int KOEFS_WIN1 = Integer.parseInt("win1");
+    public static final int KOEFS_WIN2 = Integer.parseInt("win2");
+    public static final int KOEFS_TOTALMORE = Integer.parseInt("totalMore");
+    public static final int KOEFS_TOTALLESS = Integer.parseInt("totalLess");
+
+    public static final String FINANCES_TABLE = "finances";
+    public static final int FINANCES_ID = Integer.parseInt("id");
+    public static final String FINANCES_DAY = "day";
+    public static final int FINANCES_BETSUM = Integer.parseInt("betSum");
+    public static final int FINANCES_WON = Integer.parseInt("won");
+    public static final int FINANCES_LOST = Integer.parseInt("lost");
+
+    public static final String EVENTS_TABLE = "Event";
+    public static final int EVENTS_IDTEAM1 = Integer.parseInt("idTeam1");
+    public static final String EVENTS_SPORT = "sport";
+    public static final int EVENTS_IDTEAM2 = Integer.parseInt("idTeam2");
+    public static final int KOEF_ID = Integer.parseInt("koef");
+    public static final int EVENTS_ID = Integer.parseInt("idEvent");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
