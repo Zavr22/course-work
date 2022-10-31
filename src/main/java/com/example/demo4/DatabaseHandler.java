a8e2168a4848eda84060766e4ce59fcdf92da290
norog6 <92385720+norog6@users.noreply.github.com>
2022-10-31 23:46:04 +0300

    [bd works]

--- src/main/java/com/example/demo4/DatabaseHandler.java
+++ src/main/java/com/example/demo4/DatabaseHandler.java
@@ -3,13 +3,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import java.sql.Driver;
 public class DatabaseHandler extends Configs{
     Connection dbConnection;
 
     public Connection getDbConnection()
         throws ClassNotFoundException, SQLException{
-        String connectionString = "jbdc:mysql://" + dbHost + ":"
+        String connectionString = "jdbc:mysql://" + dbHost + ":"
                 + dbPort + "/" + dbName;
         Class.forName("com.mysql.jdbc.Driver");
 
@@ -21,7 +21,7 @@ public class DatabaseHandler extends Configs{
     }
 public void signUp(String name, String surname,String password,String login )
 {
-String insert="INSERT INTO"+Constants.USER_TABLE+"("+Constants.USER_LOGIN+","+Constants.USER_PASSWORD+","+
+String insert="INSERT INTO "+Constants.USER_TABLE+"("+Constants.USER_LOGIN+","+Constants.USER_PASSWORD+","+
         Constants.USER_NAME+","+Constants.USER_SURNAME+")"+"VALUES(?,?,?,?)";
 
     try {
